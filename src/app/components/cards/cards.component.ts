d21f6636d5d73fcbc282c62a820f6b1d6263a0af
RafaGms <faelgomes2003@gmail.com>
2023-06-13 10:28:59 -0300

    Implementar o cadastro

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,5 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CardService } from 'src/app/service/card.service';
+import { Carros } from './card';
 
 @Component({
   selector: 'app-cards',
@@ -8,8 +11,40 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CardsComponent {
 
+  carros: Carros[] = [];
   formGroupAnuncio: FormGroup;
 
+  constructor(private cardservice: CardService, private formBuilder: FormBuilder) {
 
+    this.formGroupAnuncio = formBuilder.group({
+      id: [''],
+      name: [''],
+      valor: [''],
+      ano: [''],
+      descricao: [''],
+      imagem: ['']
+
+    });
+  }    // para o componente chamar o serviço
+
+  ngOnInit(): void {
+    this.loadAnuncio();
+  }
+
+  loadAnuncio() {
+    this.cardservice.getCarros().subscribe({
+      next: data => this.carros = data,                         //next pega os carros
+      error: (error) => console.log('Error ao chamar o endpoint' + error)
+    })
+  }
+
+  saveCarros() {
+    this.cardservice.saveCarros(this.formGroupAnuncio.value).subscribe({
+      next: data => {
+        this.carros.push(data);
+        this.formGroupAnuncio.reset();
+      }
+    })
+  }
 
 }
--- src/app/service/card.service.ts
+++ src/app/service/card.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CardsComponent } from '../components/cards/cards.component';
+import { Carros } from '../components/cards/card';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  url ="http://localhost:3000/carros";
+
+  getCarros():Observable <Carros[]>{
+
+    return this.http.get<Carros[]>(this.url) //metodo GET do HTTP para requisição
+  }
+
+  saveCarros(carros: Carros): Observable<Carros>{
+    return this.http.post<Carros>(this.url, carros);
+  }
+
 }
